e2eb3d98da2e32716ee86029d6562ed8d6c86c72
RANKblank36 <tendosana@gmail.com>
2025-06-21 18:35:16 +0200

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -4,7 +4,7 @@ from tkinter.filedialog import askopenfilename, asksaveasfilename
 window = Tk()
 window.title("Codingal's Text Editor")
 window.geometry("600x500")
-window.rowconfigure(0, minsize=800, weigth=1)
+window.rowconfigure(0, minsize=800, weight=1)
 window.columnconfigure(1, minsize=800, weight=1)
 
 def open_file():
@@ -38,14 +38,3 @@ def save_file():
     window.title(f"Codingal's Text Editor - {filepath}")
 
 txt_edit = Text(window)
-fr_buttons = Frame(window, relief=RAISED, bd=2)
-btn_open = Button(fr_buttons, text="Open", command=open_file)
-btn_save = Button(fr_buttons, text="Save As...", command=save_file)
-
-btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
-btn_save.grid(row=1, column=0, sticky="ew", padx=5)
-
-fr_buttons.grid(row=0, column=0, sticky="ns")
-txt_edit.grid(row=0, column=1, sticky="nsew")
-
-window.mainloop()
